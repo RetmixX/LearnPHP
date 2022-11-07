b000429e9bb7121202e6edb4273e71dd114ddba3
retmix <jameslayng123@gmail.com>
2022-11-07 18:27:07 +0700

    init

--- Lesson7/Lesson7.php
+++ Lesson7/Lesson7.php
@@ -1,13 +1,14 @@
 <?php
-$task1 = fopen("task", "r");
-$task2 = fopen("task2", "r");
+$test = [];
 
-$task3 = fopen("task", "r");
+$i = 345;
 
-echo strtoupper(fgets($task1))."\n";
-echo strtoupper(fgets($task2))."\n";
+while ($i!=358){
+    $test[]=$i;
+    $i++;
+}
+var_dump($test);
 
-$strArray = explode(" ", fgets($task3));
-foreach (array_reverse($strArray) as $item){
-    echo $item." ";
+while (true){
+    echo "Бесконечный цикл";
 }
